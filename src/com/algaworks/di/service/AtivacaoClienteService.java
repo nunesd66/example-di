0e0fd2fac24c9c811f6465d791495403f213250a
Daniel <nunes_daniel@outlook.com.br>
2022-02-02 13:29:40 -0300

    example di

---
+++ src/com/algaworks/di/service/AtivacaoClienteService.java
@@ -0,0 +1,19 @@
+package com.algaworks.di.service;
+
+import com.algaworks.di.modelo.Cliente;
+import com.algaworks.di.notificacao.Notificador;
+
+public class AtivacaoClienteService {
+
+	private Notificador notificador;
+	
+	public AtivacaoClienteService(Notificador notificador) {
+		this.notificador = notificador;
+	}
+	
+	public void ativar(Cliente cliente) {
+		cliente.ativar();
+		this.notificador.notificar(cliente, "Cadastro ativo no sistema.");
+	}
+	
+}
